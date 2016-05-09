e1a0aaf7fcf0e4e3500dc68c94ed9ad9ee1ef120
joamag <joamag@hive.pt>
2016-05-09 19:58:57 +0100

    new version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "budy_api",
-    version = "0.2.1",
+    version = "0.2.2",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Budy API Client",
