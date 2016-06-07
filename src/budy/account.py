ca746e8fec089c1072c7a2a2e0302b8b0fce6d9d
João Magalhães <joamag@hive.pt>
2016-06-07 13:01:27 +0100

    Update account.py

--- src/budy/account.py
+++ src/budy/account.py
@@ -39,12 +39,13 @@ __license__ = "Apache License, Version 2.0"
 
 class AccountApi(object):
 
-    def create_account(self, payload):
+    def create_account(self, payload, pre_enabled = True):
         url = self.base_url + "accounts"
         contents = self.post(
             url,
             auth = False,
-            data_j = payload
+            data_j = payload,
+            params = dict(pre_enabled = pre_enabled)
         )
         return contents
 
