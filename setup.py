254006a96814b89a516069b363f5979d8525181a
João Magalhães <joamag@gmail.com>
2022-06-25 12:39:36 +0100

    version: 0.3.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "budy_api",
-    version = "0.2.18",
+    version = "0.3.0",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Budy API Client",
