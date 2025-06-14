d72d82dc7a9cded3b966e8a824daef681f17eb91
João Magalhães <joamag@gmail.com>
2025-06-14 20:15:26 +0100

    Expand type stubs

---
+++ src/budy/subscription.pyi
@@ -0,0 +1,5 @@
+from typing import Any, Dict
+
+
+class SubscriptionAPI:
+    def create_subscription(self, payload: Dict[str, Any]) -> Any: ...
---
+++ src/budy/voucher.pyi
@@ -0,0 +1,11 @@
+from typing import Any
+
+class VoucherAPI:
+    def list_vouchers(self, *args: Any, **kwargs: Any) -> Any: ...
+    def create_value_voucher(
+        self,
+        amount: float,
+        key: str | None = None,
+        currency: str | None = None,
+        unlimited: bool = False,
+    ) -> Any: ...
